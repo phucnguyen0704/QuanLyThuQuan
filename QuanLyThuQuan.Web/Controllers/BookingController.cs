eefc4aa3bd6a5d6ef351d46c8825c417d7397cef
Bao <nguyengiabao91753@gmail.com>
2025-04-18 19:14:00 +0700

    add: Create Controller

---
+++ QuanLyThuQuan.Web/Controllers/BookingController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyThuQuan.Web.DTO;
+using QuanLyThuQuan.Web.Models;
+using QuanLyThuQuan.Web.Services.SeatService;
+
+namespace QuanLyThuQuan.Web.Controllers
+{
+    [Route("booking")]
+    public class BookingController : Controller
+    {
+        private readonly ISeatService _seatService;
+
+        public BookingController(ISeatService seatService)
+        {
+            _seatService = seatService;
+        }
+
+        [HttpGet]
+        [Route("viewseat")]
+        public async Task<IActionResult> ViewSeat() // Fixed return type to Task<IActionResult>
+        {
+            Response<List<Seat>> seatGetAll = await _seatService.GetAllSeats();
+            if (seatGetAll.Success == false)
+            {
+                return View("Error", seatGetAll.Message);
+            }
+            ViewBag.seats = seatGetAll.Data;
+
+            return View("ViewSeat");
+        }
+
+        [HttpGet]
+        [Route("newreservation")]
+        public IActionResult Booking()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("history")]
+        public IActionResult HistoryBooking()
+        {
+            return View();
+        }
+    }
+}
